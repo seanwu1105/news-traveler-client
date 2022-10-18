b49abbafb345c64df74f048d66896081cc1926fe
Shuang Wu <seanwu1105@gmail.com>
2022-10-17 23:56:29 -0400

    feat: add simple search page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,32 @@
 <script>
-  import { Alert } from 'flowbite-svelte'
+  import { Button, Heading, Navbar, NavUl, P, Search } from 'flowbite-svelte'
+  import { CodeBracket, MagnifyingGlass, Sun } from 'svelte-heros-v2'
 </script>
 
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<Navbar>
+  <NavUl class="order-1" />
+  <div class="flex gap-2 order-2">
+    <Button pill color="alternative" class="!p-2"
+      ><Sun class="focus:outline-none" /></Button
+    >
+    <Button
+      href="https://github.com/seanwu1105/news-traveler-client"
+      target="_blank"
+      rel="noreferrer"
+      pill
+      color="alternative"
+      class="!p-2"><CodeBracket class="focus:outline-none" /></Button
+    >
+  </div>
+</Navbar>
 
-<div class="p-8">
-  <Alert>
-    <span class="font-medium">Info alert!</span> Change a few things up and try submitting
-    again.
-  </Alert>
+<Heading tag="h1" class="text-center m-4">News Traveler</Heading>
+<P opacity={1} space="normal" size="3xl" class="text-center m-3"
+  >Gather opposite news to the same event.</P
+>
+
+<div class="m-10">
+  <Search>
+    <Button><MagnifyingGlass class="focus:outline-none mr-1" /> Search</Button>
+  </Search>
 </div>
