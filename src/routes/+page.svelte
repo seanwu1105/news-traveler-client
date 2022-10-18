20a637b513c25dd044f35dade5793c774afc370c
Shuang Wu <seanwu1105@gmail.com>
2022-10-17 21:35:05 -0400

    build: add Flowbite

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,12 @@
+<script>
+  import { Alert } from 'flowbite-svelte'
+</script>
+
 <h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+<div class="p-8">
+  <Alert>
+    <span class="font-medium">Info alert!</span> Change a few things up and try submitting
+    again.
+  </Alert>
+</div>
