51ed70ccdf74ec0c85d34602527b815a8fbd2d64
Shuang Wu <seanwu1105@gmail.com>
2022-10-20 14:23:21 -0400

    feat: add illustration for no data on search page

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
   import News from '$lib/components/News.svelte'
+  import NoData from '$lib/components/NoData.svelte'
   import type { PageData } from './$types'
 
   export let data: PageData
 </script>
 
-{#each data.results as news}
-  <News {...news} />
-{/each}
+{#if data.results.length === 0}
+  <NoData />
+{:else}
+  {#each data.results as news}
+    <News {...news} />
+  {/each}
+{/if}
