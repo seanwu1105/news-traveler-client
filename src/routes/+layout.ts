a331b4fa8d4848f304dd8a0c2df56c4c41fbf503
Shuang Wu <seanwu1105@gmail.com>
2022-10-20 01:31:08 -0400

    feat: parameterize search page

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,5 +1,5 @@
 import type { LayoutLoad } from './$types'
 
-export const load: LayoutLoad = ({ url: { pathname } }) => {
-  return { isRoot: pathname === '/' }
-}
+export const load: LayoutLoad = ({ url: { pathname } }) => ({
+  isRoot: pathname === '/',
+})
