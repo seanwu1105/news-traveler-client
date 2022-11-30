9f449c07b594ae7772ce95be9404f6da7d565197
Shuang Wu <seanwu1105@gmail.com>
2022-11-30 12:44:21 -0500

    fix: set default news article to avoid nullable value

--- src/routes/article/store.ts
+++ src/routes/article/store.ts
@@ -2,4 +2,13 @@ import type { Sentiment } from '$lib/server/get-sentiment'
 import type { News } from '$lib/server/search'
 import { writable } from 'svelte/store'
 
-export const articleNews = writable<News & { sentiment: Sentiment }>()
+export const articleNews = writable<News & { sentiment: Sentiment }>({
+  content: 'Data Expired. Please search again.',
+  sentiment: { confidence: 0, kind: 'neutral' },
+  title: 'Data Expired. Please search again.',
+  url: '#',
+  source: null,
+  publishedAt: null,
+  author: null,
+  urlToImage: null,
+})
